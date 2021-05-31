5e255988e5b3e74575ce046987d67b4394a8a6a5
yatyat2 <xowls2287@naver.com>
2021-05-31 19:49:35 +0900

    next.js _app 추가

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
